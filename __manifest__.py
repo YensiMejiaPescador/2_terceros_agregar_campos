0c5c5964caa936633ab6f24998b467225019545d
YensiMejiaPescador <ymsoftsas@gmail.com>
2023-12-22 22:09:53 -0500

    add: se modifica manifest

--- __manifest__.py
+++ __manifest__.py
@@ -20,7 +20,7 @@
 ##############################################################################
 
 {
-    'name': 'Modulo Relacion familiares',
+    'name': 'Modulo Relacion familiare',
     'version': '16.0.1.0.0',
     'author': 'Yensi Mejia',
     'maintainer': 'Yensi Mejia',
