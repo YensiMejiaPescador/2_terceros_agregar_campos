87009e58d9fe3aff3f857d82b07de2119effb79e
YensiMejiaPescador <ymsoftsas@gmail.com>
2023-12-23 20:58:02 -0500

    add: agregan campos

--- models/familia_m.py
+++ models/familia_m.py
@@ -3,6 +3,6 @@ from odoo import api, fields, models
 
 class FamiliaM(models.Model):
     _inherit = 'familia.m'
-    my_user = fields.Char(string="My Nickname")
+    my_nickname = fields.Char(string="My Nickname")
     my_age = fields.Integer(string="My Age")
     main_user = fields.Boolean(string="Is main user?")
